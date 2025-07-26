8dca63b448ec5aae27532df3394c0279d815c471
PraneethUday <Praneethp227@gmail.com>
2025-07-26 18:05:21 +0530

    Server-Client

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+import threading
+
+header = 64
+format = 'utf-8'
+bufferSize = 1024
+port = 5050
+server = socket.gethostbyname(socket.gethostname())
+addr = (server,port)
+disconnect = "end"
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(addr)
+def handleClient(conn,adddr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+
+    while connected:
+        msgLength = conn.recv(header).decode(format)
+        msgLength = int(msgLength)
+        msg = conn.recv(msgLength).decode(format)
+
+        if msg == disconnect:
+            connected = False
+
+        print(f"[{addr}] : {msg}")
+
+    conn.close()
+
+
+def acceptConnection():
+    clientCount = 0
+    server.listen()
+    print("[LISTENING] server is listening for connection ")
+    while True:
+        clientCount = clientCount + 1
+        conn,addr = server.accept()
+        thread = threading.Thread(target=handleClient,args=(conn,addr))
+        thread.start()
+        print("[ACTIVE CONNECTIONS] total connection = ",clientCount)
+
+print("[STARTING] server has started running")
+acceptConnection();
+
